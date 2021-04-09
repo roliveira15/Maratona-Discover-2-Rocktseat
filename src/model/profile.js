b33d62726048f6a1c70e37ba541ccdfa5a81694e
Ronaldo de Oliveira <ronaldo.de.oliveira@outlook.com>
2021-04-08 23:36:27 -0300

    Started the refactoring of the code

---
+++ src/model/profile.js
@@ -0,0 +1,20 @@
+
+let data = {
+        name:"Ronaldo",
+        avatar: "https://github.com/roliveira15.png",
+        "monthly-budget": 3000,
+        "days-per-week":5,
+        "hours-per-day":5,
+        "vacation-per-year": 4,
+        "value-hour":75
+    };
+
+    module.exports ={
+        get(){
+            return data;
+        },
+        update(newData){
+            data = newData;
+            return 
+        }
+    }
--- src/server.js
+++ src/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const server = express();
 const PORT = 3000; 
 const routes = require('./routes')
+const path = require('path')
+
+server.set('view engine', 'ejs')
+
+server.set('views', path.join(__dirname, 'views'))
 
 server.use(express.static("public"))
+
+// usar o re.body
+server.use(express.urlencoded({extended: true}))
 
 server.use(routes)
 
